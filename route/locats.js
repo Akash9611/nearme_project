3b87fed7d26548b8d727c2054fcd376d3284a8ae
unknown <gpkp176172@gmail.com>
2022-07-13 22:59:24 +0530

    Location Aggregation update

--- route/locats.js
+++ route/locats.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const { getLocations, addLocation, patchLocations, getNearLocations } = require('../controller/locats');
 
-router
-  .route('/')
-  .get(getLocations);
+router.get('/location',getLocations);
 router.post('/', addLocation);
 router.patch('/', patchLocations);
 router.get('/near',getNearLocations);
